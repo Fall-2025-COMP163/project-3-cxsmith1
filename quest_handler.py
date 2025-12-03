297826d4a709077168a183f5555983fbf3feeedb
cxsmith1 <cxsmith1@aggies.ncat.edu>
2025-12-02 23:35:16 -0500

    Update quest_handler.py

--- quest_handler.py
+++ quest_handler.py
@@ -1,20 +1,17 @@
-from custom_exceptions import QuestNotFoundError, InsufficientLevelError, QuestRequirementsNotMetError, QuestAlreadyCompletedError, QuestNotActiveError
+# quest_handler.py
+from custom_exceptions import QuestError, QuestNotFoundError
 
-def start_quest(character, quest_id, quests):
-    if quest_id not in quests:
-        raise QuestNotFoundError()
-    quest = quests[quest_id]
-    if character["level"] < quest.get("required_level",1):
-        raise InsufficientLevelError()
-    prereq = quest.get("prerequisite")
-    if prereq and prereq not in character["completed_quests"]:
-        raise QuestRequirementsNotMetError()
-    if quest_id in character["completed_quests"]:
-        raise QuestAlreadyCompletedError()
-    character["active_quests"].append(quest_id)
+def accept_quest(player, quest):
+    pass
 
-def complete_quest(character, quest_id, quests):
-    if quest_id not in character["active_quests"]:
-        raise QuestNotActiveError()
-    character["active_quests"].remove(quest_id)
-    character["completed_quests"].append(quest_id)
+def complete_quest(player, quest):
+    pass
+
+def abandon_quest(player, quest):
+    pass
+
+def get_active_quests(player):
+    return []
+
+def get_available_quests():
+    return []
